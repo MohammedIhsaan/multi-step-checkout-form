9669a85c3fe705dc127f190d75ba97e482d7b64d
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-03-30 12:13:26 +0530

    Multi Step Checkout from layout is created using materail UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Checkout from "./component/CheckOut";
+// import Checkout from "./component/CheckOut";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <HorizontalLinearStepper /> */}
+      <Checkout />
+    </div>
+  );
+}
+
+export default App;
